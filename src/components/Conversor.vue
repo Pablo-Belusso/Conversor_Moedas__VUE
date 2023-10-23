afca7d95820af1ba21abab1f369afb446e40d460
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-10-23 05:48:16 -0300

    Commit 14

--- src/components/Conversor.vue
+++ src/components/Conversor.vue
@@ -1,15 +1,15 @@
 <template>
-    
+ 
     <div class="conversor">
         <h2> {{ moedaA }} Para {{ moedaB }}</h2>
         <input type="text">
         <input type="button" value="Converter">
     </div>
-   
+
 </template>
 
 <script>
-
+/* eslint-disable */
     export default {
         name: 'Conversor',
         props: ["moedaA", "moedaB"]
