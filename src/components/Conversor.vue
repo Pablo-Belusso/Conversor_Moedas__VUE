1808e02e08044e661c46ddeee834490062d9e2e4
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-10-31 00:51:26 -0300

    Commit 28

--- src/components/Conversor.vue
+++ src/components/Conversor.vue
@@ -3,7 +3,7 @@
     <div class="conversor">
         <h2> {{ moedaA }} Para {{ moedaB }}</h2>
         <input type="text" v-model="moedaA_value" v-bind:placeholder="moedaA">
-        <input type="button" value="Converter">
+        <input type="button" value="Converter" v-on:click="converter">
         <h2>{{ moedaB_value }}</h2>
     </div>
 
