4f07bed4b19b89e42e8b8c476709f856d03189da
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-11-07 01:32:31 -0300

    Commit 56

--- src/components/Conversor.vue
+++ src/components/Conversor.vue
@@ -2,51 +2,43 @@
     <div class="conversor">
       <h2>{{ moedaA }} Para {{ moedaB }}</h2>
       <input type="text" v-model="moedaA_value" v-bind:placeholder="moedaA">
-      <input type="button" value="Converter" v-on:click="converter">
+      <input type="button" value="Converter" @click="converter">
       <h2>{{ moedaB_value }}</h2>
     </div>
   </template>
 
 <script>
 /* eslint-disable */
-    export default {
-        name: 'Conversor',
-        props: ["moedaA", "moedaB"],
-        data() {
-            return {
-            moedaA_value: "",
-            moedaB_value: 0
-            };
-        },
-        methods: {
+import axios from 'axios';
 
-            converter() {
-                let de_para = this.moedaA + "_" + this.moedaB;
-
-                let url = "http://localhost:3000/api/exchangerates_data/latest";
-
-                fetch(url, {
-                    headers: {
-                    'Api-Key': 'd468e25f57a2ef2a05d27ee3919375c9' // Substitua com sua chave de API da ExchangeRate-API
-                    }
-                })
-
-                .then((res) => res.json())
-                .then((data) => {
-                    console.log(data);
-                    let cotacao = data.rates[de_para];
-                    if (cotacao !== undefined) {
-                    this.moedaB_value = (cotacao * parseFloat(this.moedaA_value)).toFixed(2);
-                    } else {
-                    console.error("Taxa de câmbio não encontrada para a conversão especificada.");
-                    }
-                })
-                .catch((error) => {
-                    console.error("Erro ao buscar taxas de câmbio: ", error);
-                });
-            }
-        }
+export default {
+  name: 'Conversor',
+  props: ["moedaA", "moedaB"],
+  data() {
+    return {
+      moedaA_value: "",
+      moedaB_value: 0
     };
+  },
+  methods: {
+    converter() {
+      let de_para = this.moedaA + "_" + this.moedaB;
+      
+
+      // Faça a solicitação à API do Open Exchange Rates por meio do servidor de proxy
+      axios
+        .get(`http://localhost:8081/https://open.er-api.com/v6/latest/${de_para}.json?app_id=e984274a5030437189708b5d26ecf3cc`)
+        .then(response => {
+          const data = response.data;
+          const rate = data.rates[this.moedaB];
+          this.moedaB_value = (rate * parseFloat(this.moedaA_value)).toFixed(2);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
+};
 </script>
 
 
